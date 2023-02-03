10fde5ccb1e3ec95e660eacf5eeea8457b591762
Vishwanath <vishwa.s787@gmail.com>
2023-02-03 10:16:20 +0530

    Initial Commit

---
+++ src/test/java/com/example/javatpoint/springboothandson/SpringBootHandsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javatpoint.springboothandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHandsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
